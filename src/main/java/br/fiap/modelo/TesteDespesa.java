c9b6cb6e70065cf3ed9e1dfb9727b94df2757928
Antonio Marcos Selmini <marcos.selmini@uol.com.br>
2025-08-27 09:52:39 -0300

    Add files via upload

--- src/main/java/br/fiap/modelo/TesteDespesa.java
+++ src/main/java/br/fiap/modelo/TesteDespesa.java
@@ -18,13 +18,22 @@ public class TesteDespesa {
 //        despesa.setCategoria(new Categoria(1L, ""));
 //        dao.inserir(despesa);
 
-        List<Despesa> lista = dao.listar();
+//        List<Despesa> lista = dao.listar();
+//        for(Despesa d : lista) {
+//            System.out.println(d.getId());
+//            System.out.println(d.getDescricao());
+//            System.out.println("R$ " + d.getValor());
+//            System.out.println(d.getData().format(mascara));
+//            System.out.println(d.getCategoria().getId());
+//            System.out.println("=================================");
+//        }
+
+        List<Despesa> lista = dao.relatorio();
         for(Despesa d : lista) {
-            System.out.println(d.getId());
             System.out.println(d.getDescricao());
             System.out.println("R$ " + d.getValor());
             System.out.println(d.getData().format(mascara));
-            System.out.println(d.getCategoria().getId());
+            System.out.println(d.getCategoria().getCategoria());
             System.out.println("=================================");
         }
     }
